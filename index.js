a080384b15e799716fd3ecb6c3cc2b783eaa51e7
chirag-bharadwaj <chiragcr2one@gmail.com>
2024-02-26 18:56:23 +0530

    second

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 const routes = require('./Services');
 const bodyParser = require('body-parser')
 const user = require('./Models/user')
@@ -11,7 +12,7 @@ const user = require('./Models/user')
 app.use('/', routes);
 
 
-mongoose.connect(dbURL, {useNewUrlParser: true,useUnifiedTopology: true}).then(()=>{
+mongoose.connect(dbURL, {useNewUrlParser: true}).then(()=>{
     console.log("DB Connected...");
 }).catch((err)=>{
     console.log("Error",err);
@@ -52,6 +53,87 @@ app.get('/findUserById/:id', async (req, res)=>{
     }
 })
 
+app.post('/signUp', async (req, res)=>{
+    try {
+        const {userName, email, age, password} = req.body
+        const varifyUser = await user.findOne({userName: userName})
+        if(varifyUser)
+            return res.status(401).json({message: "User Already exist with this email"})
+
+            const hashPassword = await bcrypt.hash(password, 12)
+            const newUser = new user({userName, email, age, password: hashPassword})
+            await newUser.save()
+            return res.status(200).json({Message: "New User Created successfully", newUser})
+            
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error", error})
+    }
+    
+})
+
+app.get('/allUser', async (req, res)=>{
+    try {
+        const age = req.body.age
+        const allUsers = await user.find({age: {$lt: age} }).select('userName')
+        res.status(201).json({message: "List of Users", allUsers})
+        
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error", error})
+        
+    }
+})
+
+app.get('/userCount', async (req,res)=>{
+    try {
+        const count = await user.find({}).sort({updatedAt: 1})
+        res.status(201).json({message: "User Count is:", count})
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error", error})
+    }
+})
+
+app.put('/updateUserName/:id', async (req, res)=>{
+    const id = req.params.id
+    try {
+        const {userName} = req.body
+        const updateUser = await user.findByIdAndUpdate(id, {userName} , {new: true} )
+
+        if(!updateUser){
+            return res.status(404).json({message: "No User Found"})
+        }
+        res.status(200).json({message: "user updated successfully", updateUser})
+    } catch (error) {
+        
+    }
+})
+
+app.delete('/deleteUser/:id', async (req, res)=>{
+    const id = req.params.id
+    try {
+        const deleteUserId = await user.findByIdAndDelete(id)
+        if(!deleteUserId){
+            return res.status(404).json({message: "No User Found"})
+        }
+        res.status(200).json({message: "User Deleted successfully"})
+    } catch (error) {
+        
+    }
+})
+
+app.get('/findUserById/:id', async (req, res)=>{
+    const id = req.params.id
+    try {
+        const userId = await user.find(id)
+        if(!userId){
+            return res.status(404).json({message: "No User Found"})
+        }
+        res.status(200).json({message: "User Found", userId})
+    } catch (error) {
+        console.log("Error", error);
+        res.send(error)
+    }
+})
+
+
 
 module.exports = app;
-
